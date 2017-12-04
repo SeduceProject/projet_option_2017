520678880fe35ad2fb5048cd468bbf7d1e7a2462
unknown <mcomon15@etudiant.mines-nantes.fr>
2017-12-04 18:17:03 +0100

    tests capteurs

--- tests.py
+++ tests.py
@@ -31,7 +31,14 @@ class FlaskrTestCase(unittest.TestCase):
 		assert sensor.id == sensor2.id and sensor.mac == sensor2.mac and sensor.name == sensor2.name and sensor.state == sensor2.state and sensor.type == sensor2.type
 		delete_sensor(sensor.id)
 
-	
+	def test_modify_sensor(self):
+		data = json.dumps({"mac": "mac", "model": "model", "name": "cute sensor", "state": 0, "type": "temp"})
+		sensor = create_sensor(data)
+		databis = json.dumps({"mac": "mac change", "model": "model change", "name": "cute sensor change", "state": 1, "type": "temp change"})
+		update_sensor(sensor.id, databis)
+		sensor_change = get_sensor(sensor.id)
+		assert sensor_change.id != None and sensor_change.mac == "mac change" and sensor_change.name == "cute sensor change" and sensor_change.state == 1 and sensor_change.type == "temp change"
+		delete_sensor(sensor.id)	
 
 
 if __name__ == '__main__':
