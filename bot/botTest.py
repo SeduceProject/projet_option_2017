e7eb2795f4d51bf8d1c7d5719981b870015b052d
Manon Véber <manon.veber@etudiant.mines-nantes.fr>
2018-01-17 14:56:55 +0100

    Mise de la variable du chat courant en global, et en commentaire y'a les bouts de codes qui pourront être utile pour signaler les events

--- bot/botTest.py
+++ bot/botTest.py
@@ -4,9 +4,11 @@ import time
 import urllib
 import dbBot
 
-TOKEN = "472925886:AAHoewEo-y9D9wIaljXJllows3UNIPHOIao"
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-db = dbBot.DBBot()
+TOKEN_BOT = "472925886:AAHoewEo-y9D9wIaljXJllows3UNIPHOIao"
+URL_BOT = "https://api.telegram.org/bot{}/".format(TOKEN_BOT)
+URL_API = "localhost:8888/seduce"
+DB = dbBot.DBBot()
+CHAT = ""
 
 def get_url(url):
 	response = requests.get(url)
@@ -18,53 +20,89 @@ def get_json_from_url(url):
 	js = json.loads(content)
 	return js
 
+def set_chat(chat):
+	global CHAT
+	CHAT = chat
+
 def get_updates(offset=None):
-	url = URL + "getUpdates?timeout=100"
+	url = URL_BOT + "getUpdates?timeout=100"
 	if offset:
 		url += "&offset={}".format(offset)
 	js = get_json_from_url(url)
 	return js
-
+	
+def get_events(offset=None):
+	"""if offset:
+		#ici on met l'url de la fonction de l'api qui renvoie tous les evenements a partir de offset 
+		#url = URL_API + "/events/get_all/" + str(offset)
+	else:
+		#ici on met l'url de la fonction de l'api qui renvoie tous les evenements
+		#url = URL_API + "/events/get_all" """
+	js = get_json_from_url(url)
+	return js
+	
 def get_last_update_id(updates):
 	update_ids = []
 	for update in updates["result"]:
 		update_ids.append(int(update["update_id"]))
 	return max(update_ids)
 
-def get_last_chat_id_and_text(updates):
-	num_updates = len(updates["result"])
-	last_update = num_updates - 1
-	text = updates["result"][last_update]["message"]["text"]
-	chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-	return (text, chat_id)
-
-def answer_all(updates):
+def get_last_event_id(events):
+	event_ids = []
+	for event in events["quelqueChoseVoirDansLeFormatDuJson"]:
+		event_ids.append(int(event["id"]))
+	return max(event_ids)
+	
+def answer_all_updates(updates):
 	for update in updates["result"]:
 		try:
 			text = update["message"]["text"]
 			chat = update["message"]["chat"]["id"]
-			send_message(text, chat)
+			set_chat(chat)
+			send_message(text)
 		except Exception as e:
 			print(e)
+			
+def send_all_events(events):
+	for event in events["quelqueChoseVoirDansLeFormatDuJson"]:
+		if (event["ended"] == false):
+			try:
+				start = event["start"]
+				importance = event["importance"]
+				title = event["title"]
+				id_sensor = event["id_sensor"]
+				text = "At the moment " + str(start) + ", the sensor with the id " + str(id_sensor) + "reported an event of importance " + str(importance) + " and of title '" + title + "'."
+				send_alert(text)
+			except Exception as e:
+				print(e)
 
-def send_message(text, chat_id):
+def send_message(text):
 	if (text[:19]=="get_id_sensor/name/"):
 		name_s = text[19:]
-		print name_s
-		res = str(db.get_sensor(name_s)[0])
-		params = {"text": res, "chat_id": chat_id}
+		res = str(DB.get_sensor(name_s)[0])
+		params = {"text": res, "chat_id": CHAT}
 	else:
-		params = {"text": text, "chat_id": chat_id}
-	url = URL + "sendMessage?" + urllib.urlencode(params)
+		params = {"text": text, "chat_id": CHAT}
+	url = URL_BOT + "sendMessage?" + urllib.urlencode(params)
 	get_url(url)	
+	
+def send_alert(text):
+	params = {"text": text, "chat_id": CHAT}
+	url = URL_BOT + "sendMessage?" + urllib.urlencode(params)
+	get_url(url)
 
 def main():
 	last_update_id = None
+	#last_event_id = None
 	while True:
 		updates = get_updates(last_update_id)
 		if len(updates["result"]) > 0:
 			last_update_id = get_last_update_id(updates) + 1
-			answer_all(updates)
+			answer_all_updates(updates)
+		"""events = get_events(last_event_id)
+		if len(events["quelqueChoseVoirDansLeFormatDuJson"]) > 0:
+			last_event_id = get_last_event_id(events) + 1
+			answer_all_events(events)"""
 		time.sleep(0.5)
 
 
